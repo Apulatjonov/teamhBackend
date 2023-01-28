99ad36ce1d7f8d761af90a906d1058fd6756e7f7
abdulaziz_pulatjonov@epam.com <PulaT@7343>
2023-01-28 11:33:42 +0500

    initial push

---
+++ src/main/java/com/teamh/FuelBack/FuelBackApplication.java
@@ -0,0 +1,13 @@
+package com.teamh.FuelBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelBackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/teamh/FuelBack/entities.java
@@ -0,0 +1,4 @@
+package com.teamh.FuelBack;
+
+public class entities {
+}
